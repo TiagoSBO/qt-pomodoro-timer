d468b32a7a01d07aae6dbc23e2469769adfb0702
Tiago SBO <tiagosilvabarretto@gmail.com>
2025-04-15 16:37:59 -0300

    build: Dynamically add stylesheet in the build using qrc(resource).

--- src/main.cpp
+++ src/main.cpp
@@ -3,18 +3,35 @@
 #include <QFile>
 #include <QTextStream>
 
+//Read Qsss files
+QString loadStyleSheet(const QStringList &qssPaths) {
+    QString finalStyleSheet;
+    for (const QString &path : qssPaths) {
+        QFile file;
+        if (QFile::exists(path)) {
+            file.setFileName(path);  // External fallback
+        } else {
+            file.setFileName(":/styles/" + QFileInfo(path).fileName());  // Internal fallback
+        }
+        if (file.open(QFile::ReadOnly)) {
+            finalStyleSheet += QString::fromUtf8(file.readAll()) + "\n";
+        }
+    }
+    return finalStyleSheet;
+}
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
 
-    QFile file("C:/Users/Tiago/Documents/QtCreator_Projects/pomodoro_timer/styles/styles.qss"); // Certifique-se do caminho do arquivo
-    if (!file.open(QFile::ReadOnly)) {
-        qDebug() << "Erro ao abrir styles.qss: " << file.errorString();
-    } else {
-        QString styleSheet = QLatin1String(file.readAll());
-        a.setStyleSheet(styleSheet);
-    }
-    file.close();
+    QStringList styleSheets = {
+        ":/styles/styles/timer.qss",
+        ":/styles/styles/sessionlogs.qss",
+        ":/styles/styles/settingsdialog.qss"
+    };
+
+    QString combinedStyle = loadStyleSheet(styleSheets);
+    a.setStyleSheet(combinedStyle);
 
     MainWindow w;
     w.show();
